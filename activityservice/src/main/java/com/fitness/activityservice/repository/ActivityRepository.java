cdf9bf8f3bf8533a67cbc8d7ac5888196671e86a
Loukik Tayshete <52589175+gamechanger2580@users.noreply.github.com>
2025-09-06 16:18:06 +0530

    Delete activityservice directory (adding a new one)

--- activityservice/src/main/java/com/fitness/activityservice/repository/ActivityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fitness.activityservice.repository;
-
-import com.fitness.activityservice.model.Activity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ActivityRepository extends MongoRepository<Activity, String> {
-    
-}
